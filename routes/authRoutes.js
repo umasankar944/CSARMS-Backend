9c0c51d883ab01fd7678e1a8adc00f4f28da0b0e
APramod <153256566+APramod2002@users.noreply.github.com>
2024-12-04 19:52:48 +0530

    category backend
    category backend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { login, register, getUserDetails } from "../controllers/authController.js";
+import { createCategory, deleteCategory, getCategories, updateCategory } from "../controllers/categoryController.js";
 
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/getUserDetails", getUserDetails)
+router.get("/categories", getCategories);
+router.post("/categories", createCategory);
+router.put("/categories/:categoryId", updateCategory);
+router.delete("/categories/:categoryId", deleteCategory);
 export default router;
